3507fc77fd46621b405ce4983a8ab03a87b1e903
BasicallyIAmFox <65787578+BasicallyIAmFox@users.noreply.github.com>
2026-01-29 11:12:56 +0100

    updated sim

--- vietas-formula/simulation.ts
+++ vietas-formula/simulation.ts
@@ -17,76 +17,61 @@ class vfSim extends theoryClass<theory> {
   t_var: number;
   
   getBuyingConditions(): conditionFunction[] {
+    const activeStrat = [
+      /* tdot */ true,
+      /* a1 */ () => this.variables[1].cost + l10(10) < this.variables[2].cost,
+      /* b1 */ true,
+      /* a2 */ () => this.variables[3].cost + l10(10) < this.variables[4].cost,
+      /* b2 */ true,
+      /* a3 */ () => this.variables[5].cost + l10(10) < this.variables[6].cost,
+      /* b3 */ true,
+      /* a4 */ () => this.variables[7].cost + l10(10) < this.variables[8].cost,
+      /* b4 */  true,
+      /* a5 */ () => this.variables[9].cost + l10(10) < this.variables[10].cost,
+      /* b5 */ true
+    ];
+
     const conditions: Record<stratType[theory], (boolean | conditionFunction)[]> = {
-      VF: [
-        true,
-        true,
-        true,
-        true,
-        true,
-        true,
-        true,
-        true,
-        true,
-        true,
-        true
-      ],
-      VFd: [
-        true,
-        () => this.variables[1].cost + l10(10) < this.variables[2].cost,
-        true,
-        () => this.variables[3].cost + l10(10) < this.variables[4].cost,
-        true,
-        () => this.variables[5].cost + l10(10) < this.variables[6].cost,
-        true,
-        () => this.variables[7].cost + l10(10) < this.variables[8].cost,
-        true,
-        () => this.variables[9].cost + l10(10) < this.variables[10].cost,
-        true
-      ],
-      VFdMS: [
-        true,
-        () => this.variables[1].cost + l10(10) < this.variables[2].cost,
-        true,
-        () => this.variables[3].cost + l10(10) < this.variables[4].cost,
-        true,
-        () => this.variables[5].cost + l10(10) < this.variables[6].cost,
-        true,
-        () => this.variables[7].cost + l10(10) < this.variables[8].cost,
-        true,
-        () => this.variables[9].cost + l10(10) < this.variables[10].cost,
-        true
-      ]
+      VF: new Array(11).fill(true),
+      VFMS: activeStrat
     };
     return toCallables(conditions[this.strat]);
   }
   getVariableAvailability(): conditionFunction[] {
     const conditions: conditionFunction[] = [
-      () => this.variables[0].level < 4,
-      () => true,
-      () => true,
-      () => true,
-      () => true,
-      () => true,
-      () => this.milestones[1] > 0,
-      () => this.milestones[0] > 0,
-      () => this.milestones[1] > 1,
-      () => this.milestones[0] > 1,
-      () => this.milestones[1] > 2
+      /* tdot*/ () => this.variables[0].level < 4,
+      /* a1 */ () => true,
+      /* b1 */ () => true,
+      /* a2 */ () => true,
+      /* b2 */ () => true,
+      /* a3 */ () => true,
+      /* b3 */ () => this.milestones[1] > 0,
+      /* a4 */ () => this.milestones[0] > 0,
+      /* b4 */ () => this.milestones[1] > 1,
+      /* a5 */ () => this.milestones[0] > 1,
+      /* b5 */ () => this.milestones[1] > 2
     ];
     return conditions;
   }
   getTotMult(val: number): number {
-    return Math.max(0, val * this.tauFactor * 0.3 + l10(5));
+    return Math.max(0, val * 0.15 + l10(5));
   }
   getMilestonePriority(): number[] {
     const rho = Math.max(this.lastPub, this.maxRho);
 
-    const ai_points = [20, 150];
-    const bi_points = [15, 50, 200];
+    const ai_points = [20, 45];
+    const bi_points = [15, 35, 60];
     const ai_max = binaryInsertionSearch(ai_points, rho);
     const bi_max = binaryInsertionSearch(bi_points, rho);
     this.milestonesMax = [ai_max, bi_max, 5];
+
+    if (this.strat.includes("MS") && rho >= 15 && rho < 300) {
+      if (this.ticks % 20 < 10) {
+        return [0, 1, 2];
+      } else {
+        return [0, 2, 1];
+      }
+    }
 
     return [0, 2, 1];
   }
@@ -100,15 +85,15 @@ class vfSim extends theoryClass<theory> {
     this.variables = [
         new Variable({ name: "tdot", cost: new ExponentialCost(1e5, 1e5), valueScaling: new ExponentialValue(10) }),
         new Variable({ name: "a1", cost: new FirstFreeCost(new ExponentialCost(2, 3)), valueScaling: new StepwisePowerSumValue() }),
-        new Variable({ name: "b1", cost: new ExponentialCost(1e3, 500), valueScaling: new ExponentialValue(1.3) }),
+        new Variable({ name: "b1", cost: new ExponentialCost(1e3, 500), valueScaling: new ExponentialValue(2) }),
         new Variable({ name: "a2", cost: new ExponentialCost(30, 5), valueScaling: new StepwisePowerSumValue() }),
-        new Variable({ name: "b2", cost: new ExponentialCost(1e3, 600), valueScaling: new ExponentialValue(1.3) }),
+        new Variable({ name: "b2", cost: new ExponentialCost(1e3, 550), valueScaling: new ExponentialValue(2) }),
         new Variable({ name: "a3", cost: new ExponentialCost(1e4, 15), valueScaling: new StepwisePowerSumValue() }),
-        new Variable({ name: "b3", cost: new ExponentialCost(1e5, 700), valueScaling: new ExponentialValue(1.3) }),
-        new Variable({ name: "a4", cost: new ExponentialCost(1e15, 30), valueScaling: new StepwisePowerSumValue() }),
-        new Variable({ name: "b4", cost: new ExponentialCost(1e25, 800), valueScaling: new ExponentialValue(1.3) }),
-        new Variable({ name: "a5", cost: new ExponentialCost(1e150, 60), valueScaling: new StepwisePowerSumValue() }),
-        new Variable({ name: "b5", cost: new ExponentialCost(1e160, 900), valueScaling: new ExponentialValue(1.3) })
+        new Variable({ name: "b3", cost: new ExponentialCost(1e5, 600), valueScaling: new ExponentialValue(2) }),
+        new Variable({ name: "a4", cost: new ExponentialCost(1e20, 35), valueScaling: new StepwisePowerSumValue() }),
+        new Variable({ name: "b4", cost: new ExponentialCost(1e25, 650), valueScaling: new ExponentialValue(2) }),
+        new Variable({ name: "a5", cost: new ExponentialCost(1e45, 90), valueScaling: new StepwisePowerSumValue(0.1) }),
+        new Variable({ name: "b5", cost: new ExponentialCost(1e50, 700), valueScaling: new ExponentialValue(2) })
     ];
     this.updateMilestones();
   }
@@ -135,30 +120,24 @@ class vfSim extends theoryClass<theory> {
     return this.createResult();
   }
   tick() {
-    if (this.strat.includes("MS") && this.lastPub >= 15 && this.lastPub < 300) {
-      if (this.ticks % 20 < 10) {
-        this.milestones[1] += this.milestones[2];
-        this.milestones[2] = Math.max(this.milestones[1] - 3, 0);
-      } else {
-        this.milestones[2] += this.milestones[1];
-        this.milestones[1] = Math.max(this.milestones[2] - 5, 0);
-      }
-    }
-
     const aiExp = 1 + 0.03 * this.milestones[2];
-    const r0 = this.variables[1].value * aiExp + this.variables[2].value;
-    const r1 = this.variables[3].value * aiExp + this.variables[4].value;
-    const r2 = this.variables[5].value * aiExp + this.variables[6].value;
-    const r3 = this.variables[7].value * aiExp + this.variables[8].value;
-    const r4 = this.variables[9].value * aiExp + this.variables[10].value;
+    let r0 = this.variables[1].value * aiExp + this.variables[2].value;
+    let r1 = this.variables[3].value * aiExp + this.variables[4].value;
+    let r2 = this.variables[5].value * aiExp;
+    if (this.milestones[1] > 0) r2 += this.variables[6].value;
+    let r3 = this.variables[7].value * aiExp;
+    if (this.milestones[1] > 1) r3 += this.variables[8].value;
+    let r4 = this.variables[9].value * aiExp;
+    if (this.milestones[1] > 2) r4 += this.variables[10].value;
+
     let e0 = 0, e1 = 0, e2 = 0, e3 = 0, e4 = 0;
-    if (this.variables[9].level > 0) {
+    if (this.milestones[0] > 1 && this.variables[9].level > 0) {
         e0 = add(r0, r1, r2, r3, r4);
         e1 = add(r0 + add(r1, r2, r3, r4), r1 + add(r2, r3, r4), r2 + add(r3, r4), r3 + r4);
         e2 = add(r0 + add(r1 + add(r2, r3, r4), r2 + add(r3, r4), add(r3, r4)), r1 + add(r2 + add(r3, r4), r3 + r4), r2 + r3 + r4);
         e3 = add(r0 + add(r1 + add(r2 + add(r3, r4), r3 + r4), r2 + r3 + r4), r1 + r2 + r3 + r4);
         e4 = r0 + r1 + r2 + r3 + r4;
-    } else if (this.variables[7].level > 0) {
+    } else if (this.milestones[0] > 0 && this.variables[7].level > 0) {
         e0 = add(r0, r1, r2, r3);
         e1 = add(r0 + add(r1, r2, r3), r1 + add(r2, r3), r2 + r3);
         e2 = add(r0 + add(r1 + add(r2, r3), r2 + r3), r1 + r2 + r3);
@@ -178,5 +157,7 @@ class vfSim extends theoryClass<theory> {
 
     const rhodot = this.totMult + l10(this.t_var) + e0*2/2 + e1*2/3 + e2*2/4 + e3*2/5 + e4*2/6;
     this.rho.add(rhodot + l10(this.dt));
+
+    this.tauFactor = (Math.log(1 + this.maxRho) / Math.log(600)) / Math.log(this.maxRho);
   }
 }
